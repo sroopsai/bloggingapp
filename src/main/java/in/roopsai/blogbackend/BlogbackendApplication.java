85fb163874a885401626436e967168e4f349b3cd
roopsai <roopsai@outlook.in>
2024-10-24 15:31:06 +0530

    Project Setup

---
+++ src/main/java/in/roopsai/blogbackend/BlogbackendApplication.java
@@ -0,0 +1,13 @@
+package in.roopsai.blogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogbackendApplication.class, args);
+	}
+
+}
